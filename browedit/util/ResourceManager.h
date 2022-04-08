88c898c329e38d96032d3423f24f83070ef49a9d
Johan Talboom <borfje@gmail.com>
2022-04-08 23:31:06 +0200

    Resource manager now cleans up memory when items are not used anymore

--- browedit/util/ResourceManager.h
+++ browedit/util/ResourceManager.h
@@ -11,8 +11,8 @@ namespace util
 	template<class T>
 	class ResourceManager
 	{
-		static inline std::vector<T*> resources;
-		static inline std::map<std::string, T*> resmap;
+		static inline std::vector<std::pair<T*, int> > resources;
+		static inline std::map<std::string, std::pair<T*, int>> resmap;
 		static inline std::mutex loadMutex;
 	public:
 		template<class R = T>
@@ -21,12 +21,12 @@ namespace util
 			const std::lock_guard<std::mutex> lock(loadMutex);
 			for (auto r : resources)
 			{
-				R* rr = dynamic_cast<R*>(r);
+				R* rr = dynamic_cast<R*>(r.first);
 				if (rr)
 					return rr;
 			}
 			R* rr = new R();
-			resources.push_back(rr);
+			resources.push_back(std::pair<T*, int>(rr,1));
 			return rr;
 		}
 		template<class R = T>
@@ -34,19 +34,22 @@ namespace util
 		{
 			const std::lock_guard<std::mutex> lock(loadMutex);
 			auto it = resmap.find(str);
-			if(it != resmap.end())
-				return dynamic_cast<R*>(it->second);
+			if (it != resmap.end())
+			{
+				it->second.second++;
+				return dynamic_cast<R*>(it->second.first);
+			}
 			R* r = new R(str);
-			resmap[str] = r;
+			resmap[str] = std::pair<T*, int>(r, 1);
 			return r;
 		}
 
 		static void clear()
 		{
 			for (auto it : resmap)
-				delete it.second;
+				delete it.second.first;
 			for (auto res : resources)
-				delete res;
+				delete res.first;
 			resmap.clear();
 			resources.clear();
 		}
@@ -56,9 +59,9 @@ namespace util
 		{
 			std::vector<T*> ret;
 			for (auto it : resmap)
-				ret.push_back(it.second);
+				ret.push_back(it.second.first);
 			for (auto res : resources)
-				ret.push_back(res); //TODO: do this with iterator insert
+				ret.push_back(res.first); //TODO: do this with iterator insert
 			return ret;
 		}
 
@@ -66,9 +69,37 @@ namespace util
 		{
 			return resources.size() + resmap.size();
 		}
-		static void unload(T*)
+		static void unload(T* res)
 		{
-			std::cout << "Unloading resource, but not really :P this is a memory leak, and will be fixed later" << std::endl;
+			for (auto kv = resmap.begin(); kv != resmap.end(); kv++)
+			{
+				if (kv->second.first == res)
+				{
+					kv->second.second--;
+					if (kv->second.second == 0)
+					{
+						std::cout << "Resource not used anymore, unloading" << std::endl;
+						delete kv->second.first;
+						resmap.erase(kv);
+					}
+					return;
+				}
+			}
+			for (auto v = resources.begin(); v != resources.end(); v++)
+			{
+				if (v->first == res)
+				{
+					v->second--;
+					if (v->second == 0)
+					{
+						std::cout << "Resource not used anymore, unloading" << std::endl;
+						delete v->first;
+						resources.erase(v);
+					}
+					return;
+				}
+			}
+			std::cout << "Unloading resource, could not find it in the resource map!" << std::endl;
 		}
 	};
 
