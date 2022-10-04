6d3d744e62f01ce2a8619509c915c1fa728e6571
Johan Talboom <borfje@gmail.com>
2022-10-04 23:27:54 +0200

    Added global color saving

--- browedit/windows/ColorEditWindow.cpp
+++ browedit/windows/ColorEditWindow.cpp
@@ -23,6 +23,7 @@ void BrowEdit::showColorEditWindow()
 
 	auto rsw = activeMapView->map->rootNode->getComponent<Rsw>();
 
+	ImGui::PushID("MapColor");
 	if (ImGui::CollapsingHeader("Map Colors", ImGuiTreeNodeFlags_DefaultOpen))
 	{
 		if (ImGui::CollapsingHeader("Saving", ImGuiTreeNodeFlags_DefaultOpen))
@@ -58,11 +59,48 @@ void BrowEdit::showColorEditWindow()
 
 		}
 	}
+	ImGui::PopID();
 	ImGui::Spacing();
+
+	ImGui::PushID("Global Colors");
 	if (ImGui::CollapsingHeader("Global Colors", ImGuiTreeNodeFlags_DefaultOpen))
 	{
-	}
+		if (ImGui::CollapsingHeader("Saving", ImGuiTreeNodeFlags_DefaultOpen))
+		{
+			static char category[100];
+			static char name[100];
+			ImGui::InputText("Category", category, 100);
+			ImGui::InputText("Name", name, 100);
+			if (ImGui::Button("Save"))
+			{
+				config.colorPresets[category][name] = colorEditBrushColor;
+				config.save();
+			}
+			ImGui::Spacing();
+		}
 
+		float window_visible_x2 = ImGui::GetWindowPos().x + ImGui::GetWindowContentRegionMax().x;
+		ImGuiStyle& style = ImGui::GetStyle();
+		for (const auto cat : config.colorPresets)
+		{
+			if (!ImGui::CollapsingHeader((cat.first + "##").c_str(), ImGuiTreeNodeFlags_DefaultOpen))
+				continue;
+			for (const auto color : cat.second)
+			{
+				if (ImGui::ColorButton((color.first + "##").c_str(), ImVec4(color.second.r, color.second.g, color.second.b, color.second.a), 0, ImVec2(30, 30)))
+				{
+					colorEditBrushColor = color.second;
+				}
+				float last_button_x2 = ImGui::GetItemRectMax().x;
+				float next_button_x2 = last_button_x2 + style.ItemSpacing.x + 30;
+				if (next_button_x2 < window_visible_x2)
+					ImGui::SameLine();
+			}
+			ImGui::Spacing();
+
+		}
+	}
+	ImGui::PopID();
 
 
 	ImGui::End();
