9ab3f914734f336b7e754f003a10d019771ad872
Borf <borfje@gmail.com>
2023-02-20 22:12:48 +0100

    Added time on undo window (and capped amount of undo visible in undo window) and flipped undo window order

--- browedit/windows/UndoWindow.cpp
+++ browedit/windows/UndoWindow.cpp
@@ -8,18 +8,24 @@ void BrowEdit::showUndoWindow()
 	ImGui::Begin("Undo stack", &windowData.undoVisible);
 	if (activeMapView && ImGui::BeginListBox("##stack", ImGui::GetContentRegionAvail()))
 	{
-		for (auto action : activeMapView->map->undoStack)
-		{
-			ImGui::Selectable(action->str().c_str());
-		}
 		ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.9f, 0.1f, 0.1f, 1.0f));
-		for (auto action : activeMapView->map->redoStack)
+		for (auto it = activeMapView->map->redoStack.rbegin(); it != activeMapView->map->redoStack.rend(); it++)
 		{
-			ImGui::Selectable(action->str().c_str());
+			ImGui::Selectable(((*it)->time + " " + (*it)->str()).c_str());
 		}
 		ImGui::PopStyleColor();
-		if (ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
-			ImGui::SetScrollHereY(1.0f);
+
+		int c = 0;
+		for (auto it = activeMapView->map->undoStack.rbegin(); it != activeMapView->map->undoStack.rend(); it++)
+		{
+			ImGui::Selectable(((*it)->time + " " + (*it)->str()).c_str());
+			c++;
+			if (c > 100)
+			{
+				ImGui::Selectable("...");
+				break;
+			}
+		}
 		ImGui::EndListBox();
 	}
 	ImGui::End();
